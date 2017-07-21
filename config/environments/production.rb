23e95a0e8adaf0e8e2c06d218e103ddc1f803193
parallelParka <bhickey77@gmail.com>
2017-07-21 21:49:36 +0000

    Config change 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'billhickey.io'
+  host = 'www.billhickey.io'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
