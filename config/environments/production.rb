03ff220e1b11fe8e6342199a5830f5e83d457e4a
parallelParka <bhickey77@gmail.com>
2017-07-21 21:46:18 +0000

    Prod config changes for production debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'billhickey.io',
     :enable_starttls_auto => true
   }
 
