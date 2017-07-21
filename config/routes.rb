d5b05697c31ac9fdbb5608cd0b90ebdaa4a95b27
parallelParka <bhickey77@gmail.com>
2017-07-21 22:08:57 +0000

    change root url

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'users/new'
 
-  root 'static_pages#home'
+  root 'billhickey.io'
   get   '/help',     to: 'static_pages#help'
   get   '/about',    to: 'static_pages#about'
   get   '/contact',  to: 'static_pages#contact'
