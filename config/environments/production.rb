46b9d4c7a58695452e82e4825ae01b08a04b84ed
parallelParka <bhickey77@gmail.com>
2017-07-23 20:47:36 +0000

    config file move to main

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,6 +101,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
+
 
   config.action_view.default_url_options = { host: host }
+end
