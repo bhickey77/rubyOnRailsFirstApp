c9c6436033323285ef81f5dbbe6cfb5992b59e04
Bill Hickey <bhickey77@gmail.com>
2018-05-29 20:42:00 -0500

    change the host in production env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'www.billhickey.io'
+  host = 'www.rails-practice-app-blast.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
